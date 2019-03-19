1d8bd057ba48a39c038ea88ca2921e7c03b98f1b
Kim Jin (qinjin) <qinjin@cisco.com>
2019-03-19 14:53:29 +0800

    Add SmartApplicationListener demo

---
+++ event-async-smart-listener/src/main/java/com/example/demo/event/smartlistener/DemoSmartListener.java
@@ -0,0 +1,37 @@
+package com.example.demo.event.smartlistener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.event.SmartApplicationListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class DemoSmartListener implements SmartApplicationListener {
+    @Override
+    public boolean supportsEventType(Class<? extends ApplicationEvent> eventType) {
+        log.debug("--->eventType:={}", eventType);
+        return eventType == DemoMessageEvent.class || eventType == DemoTimeEvent.class;
+    }
+
+    @Override
+    public boolean supportsSourceType(Class<?> sourceType) {
+        log.debug("--->sourceType:={}", sourceType);
+        return sourceType == AnnotationConfigApplicationContext.class || sourceType == DemoPublisher.class;
+    }
+
+    @Async("defaultTaskExecutor")
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (event instanceof DemoMessageEvent) {
+            DemoMessageEvent demoMessageEvent = (DemoMessageEvent) event;
+            log.info("--->{}", demoMessageEvent.getMessage());
+        } else if (event instanceof DemoTimeEvent) {
+            DemoTimeEvent demoTimeEvent = (DemoTimeEvent) event;
+            log.info("--->{}", demoTimeEvent.getDateTime());
+        }
+    }
+
+}
